90b429b7bd04cf7675a3fae721392367fcd6cfc3
Bhuwan Tiwari <bhuwantiwari2002@gmail.com>
2022-10-19 22:32:08 -0500

    Added some more random code

--- Scripts/stopwatch.js
+++ Scripts/stopwatch.js
@@ -1,16 +1,23 @@
 const stopwatchCounterText = document.querySelector("#stopwatchCounterText");
 const startBtn = document.querySelector("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
+let startTime;
+startBtn.onclick = function(){
+    let time = new Date();
+    startTime = time.getHours() + time.getMinutes() + time.getSeconds();
+    const intervalID = setInterval(startStopWatch, 500);
+}; 
 
-const intervalID = setInterval(startStopWatch, 500);
 function startStopWatch() 
 {
     try
     {
         let today = new Date();
-        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        stopwatchCounterText.textContent = time;
-
+        let currentTime = today.getHours() + today.getMinutes() + today.getSeconds();
+        let placeHolder = new Date(currentTime - startTime);
+        let answer = new Date();
+        answer = placeHolder.getHours + ":" + placeHolder.getMinutes + ":" + placeHolder.getSeconds;
+        stopwatchCounterText.textContent = answer;
     } catch {
 
     }
